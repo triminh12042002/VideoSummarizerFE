76f40a320c56733ce6d6d5f4c765201c966591cc
triminh12042002 <triminh12042002@gmail.com>
2024-09-14 18:28:37 +0700

    feat: sign up,login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
+import HomePage from './component/home/HomePage'
+import LoginPage from './component/auth/LoginPage'
+import RegisterPage from './component/auth/RegisterPage'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <div className='content'>
+          <Routes>
+            {/* Public Routes */}
+            <Route exact path="/home" element={<HomePage />} />
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+
+            {/* Fallback Route */}
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/component/home/UploadVideo.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Upload, Button } from "antd";
+// import "/home/tri/Uni/Java/Learning/SpringBoot/Project/VideoSummarizerFE/video-summarizer-fe/node_modules/video-react/dist/video-react.css";
+import { Player } from 'video-react';
+// import '../../../video-react/dist/video-react.css';
+
+export default function UploadComponent(props) {
+    // const initialstate = {
+    //     videoSrc: ""
+    // }
+
+    const [videoSrc, setVideoSrc] = useState("");
+    // const { videoSrc } = apiData;
+
+
+    const handleChange = ({ file }) => {
+        var reader = new FileReader();
+        console.log(file)
+        var url = URL.createObjectURL(file.originFileObj);
+        setVideoSrc(url);
+    }
+
+
+    return (
+        <React.Fragment>
+            <div className="action">
+                <Upload className="mt-3 mb-3"
+                    accept=".mp4"
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture"
+                    maxCount={1}
+                    onChange={handleChange}>
+                    <Button>
+                        Upload
+                    </Button>
+                </Upload>
+
+                {/* <video width="400" controls>
+                    <source
+                        src={videoSrc.Src}
+                        type={videoSrc.type}
+                    />
+                    Your browser does not support HTML5 video.
+                </video> */}
+                <Player
+                    playsInline
+                    src={videoSrc}
+                    fluid={false}
+                    width={480}
+                    height={272}
+                />
+            </div>
+
+
+
+        </React.Fragment>
+    )
+}
